a80d717abd0b63411650048779c595a88baf1204
haejiii <gowl4622@gmail.com>
2024-04-30 15:26:54 +0900

    feat: add global sass file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@ import '@fontsource/merriweather'
 import './src/normalize.css'
 // custom CSS styles
 import './src/style.css'
+import './src/assets/css/global.scss'
 
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css'
