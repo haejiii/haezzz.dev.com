b50855e65628247f3356e1fa660c4f6cd20c5728
haejiii <gowl4622@gmail.com>
2024-05-21 14:28:32 +0900

    refactor: prism 관련 라이브러리 제거

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,6 @@
 // custom typefaces
 import '@fontsource-variable/montserrat'
 import '@fontsource/merriweather'
+
 // custom CSS styles
 import './src/style/main.scss'
-
-// Highlighting for code blocks
-import 'prismjs/themes/prism.css'
